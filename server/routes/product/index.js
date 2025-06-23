771355738d31648fbc5050aed59d58e67348ee77
Nguyen Huu Duc <minhduchg454@gmail.com>
2025-06-23 07:32:16 +0700

    cap nhat

--- server/routes/product/index.js
+++ server/routes/product/index.js
@@ -3,14 +3,17 @@ const brandRoutes = require('./branchRoutes')
 const productCategoryRoutes = require('./productCategoryRoutes')
 const productRoutes = require('./productRoutes')
 const couponRoutes = require('./couponRoutes')
-const couponProductVariation = require('../../models/product/couponProductVariation')
+const couponProductVariation = require('./couponProductVariationRoutes')
+//const productVariation = require('./productVariationRoutes')
 
+// Hàm khởi tạo các route liên quan đến sản phẩm
 const initProductRoutes = (app) => {
     app.use('/api/brands', brandRoutes)
     app.use('/api/productCategories', productCategoryRoutes)
     app.use('/api/products', productRoutes)
     app.use('/api/coupons', couponRoutes)
     app.use('/api/coupon-product-variations', couponProductVariation)
+    //app.use('/api/product-variations', productVariation)
 }
 
 module.exports = initProductRoutes
