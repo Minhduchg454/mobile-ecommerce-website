88b413a2f02d196ecd55d1c9d9ecadc4566e9fe4
Nguyen Huu Duc <minhduchg454@gmail.com>
2025-07-08 14:40:40 +0700

    update header

--- client/src/pages/public/Home.js
+++ client/src/pages/public/Home.js
@@ -19,8 +19,13 @@ const Home = ({ navigate }) => {
   const { categories } = useSelector((state) => state.app);
 
   return (
-    <div className="w-full px-4">
-      <div className="my-8 w-main m-auto ">
+    <div className="w-full px-2">
+      {/* 
+        De co gian duoc phai dung md:w-main khong dong w-main
+        + W-MAIN la no khoa chet chieu rong do, khong duoc doi du man hinh nho hay lon
+        + Chỉ từ md trở lên mới fix 1100px, còn nhỏ hơn thì auto → ✅ Co giãn
+      */}
+      <div className="md:w-main m-auto">
         <CategoryList />
       </div>
       <div className="md:w-main m-auto flex flex-col md:flex-row mt-6 py-2">
@@ -32,19 +37,19 @@ const Home = ({ navigate }) => {
           <BestSeller />
         </div>
       </div>
-      <div className="flex flex-col my-8 w-main m-auto">
+      <div className="flex flex-col my-8 md:w-main m-auto">
         <FeatureProducts
           title="ĐIỆN THOẠI ĐƯỢC ĐÁNH GIÁ CAO"
           query={{ sort: "-rating", categoryId: "6855ba0fdffd1bd4e14fb9ff" }}
         />
       </div>
-      <div className="flex flex-col my-8 w-main m-auto">
+      <div className="flex flex-col my-8 md:w-main m-auto">
         <FeatureProducts
           title="PHỤ KIỆN ĐIỆN THOẠI ĐƯỢC ĐÁNH GIÁ CAO"
           query={{ sort: "-rating", categoryId: "6855ba84634ab410b39b8bce" }}
         />
       </div>
-      <div className="flex flex-col flex-wrap my-8 w-main m-auto">
+      <div className="flex flex-col flex-wrap my-8 md:w-main m-auto">
         <FeatureInfo />
       </div>
     </div>
